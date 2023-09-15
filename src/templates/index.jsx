f40ce640933c4c371b6a148c110e5369fc903b06
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-09-15 21:44:00 +0700

    modified package.json

---
+++ src/templates/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from "rendi-ui";
+
+const Templates = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default Templates
