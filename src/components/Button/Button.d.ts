3923ea69ca9172453a5e6015ed73145e9c09e862
Rendi Purnomo <107144802+rendipurnomo@users.noreply.github.com>
2023-09-15 23:40:34 +0700

    #	new file:   src/components/Button/Button.d.ts #	modified:   src/components/client.d.ts #	modified:   src/components/package-lock.json #	modified:   src/components/package.json

--- src/components/Button/Button.d.ts
+++ src/components/Button/Button.d.ts
@@ -1,8 +1,11 @@
-
 declare type ButtonProps = {
   children?: string;
   variant?: 'contained' | 'outlined';
   color?: 'primary' | 'secondary' | 'success' | 'warning' | 'disabled';
-}
+};
 declare const Button: (props: ButtonProps) => JSX.Element;
 export default Button;
+// Context from Function d:/practice/rwid/RendiUi/src/components/Button/index.jsx:Button
+// const Button = (props) => {
+//   const { children='button', variant='contained',color='primary'} = props;
+//}
